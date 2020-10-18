112a75f014bd66ce72e68e9e137bc5d5da0dc7c5
yenchihliao <jeff.yenchih.liao@gmail.com>
2020-10-18 14:38:13 +0800

    feat: implemented node duration variable into the experiment

--- Node.py
+++ Node.py
@@ -3,7 +3,7 @@ from time import sleep
 # from AbstractNodeFactory import *
 from Message import SCPMessage
 
-# In charge of protocol it self
+# In charge of protocol itself
 # Network delay simulation, timeout strategy, and quorum(peers and trust)
 #       are delegated to other objects
 class Node():
@@ -16,38 +16,47 @@ class Node():
         self.mValue = self._getCandidate(False) # TODO
         self.mTimer = self.mFactory.createTimer()
         self.mConn = self.mFactory.createConn()
+        self.mDuration = self.mFactory.createLifeTime()
+        self._mEventDurationExpire = Event()
+        self.mLog = ''
     def _getCandidate(self, hasTimeout): # TODO
         return 1
     def _isNewerMsg(msg): # TODO
         pass
     def run(self):
+        timer = Timer(self.mDuration, self._mEventDurationExpire.set)
+        timer.start()
         self.mTimer.set(hasTimeout=False)
         self.mTimer.start()
-        while(True):
+        while(not self._mEventDurationExpire.is_set()):
             sleep(0.5)
             if(not self.mTimer.fired()):
-                print('N')
+                self.log('{} waiting ractified statement'.format(self.mNodeID))
                 if(self.mConn.ractify(self.mVotes, self.mView, self.mValue)):
                     self.mTimer.cancel()
-                    print('Ractified', '@', self.mView)
+                    self.log('Ractified @ {}'.format(self.mView))
                     self.changeView(hasTimeout=False)
                     self.mTimer.start()
                     continue
             else:
-                print('Y')
+                self.log('{} timer fired'.format(self.mNodeID))
                 self.changeView(hasTimeout=True)
                 self.mTimer.start()
+        self.log('Node duration expired')
+        print(self.mLog)
     def recv(self, msg):
         # if(self.mVotes[nodeID].mView >= msg.view)
         if(self._isNewerMsg(msg)):
             mVotes[msg.mSender] = msg.mVote
         msg.show()
-    def log(self):
-        pass
+    def log(self, output):
+        # print(output)
+        self.mLog += output
+        self.mLog += "\n"
     def broadcast(self, msg):
         self.mConn.broadcast(msg)
     def changeView(self, hasTimeout):
-        print('calling view change @', self.mView, hasTimeout)
+        self.log('calling view change @{}, hasTimeout: {}'.format(self.mView, hasTimeout))
         self.mView += 1
         self.mValue = self._getCandidate(hasTimeout)
         self.mTimer.set(hasTimeout)
@@ -60,9 +69,7 @@ if __name__ == '__main__':
     import ConnStrat
     nodeID = 1
     peers = {1, 2, 3, 4}
-    connect = ConnStrat.UniformConn(DelayStrat.NoDelay())
-    connect.initWithPeers(peers)
     factory = NodeFactory.SimpleNodeFactory()
-    factory.setConn(connect)
+    factory.createConn().initWithPeers(peers)
     node = Node(factory, nodeID)
     node.run()
--- NodeFactory.py
+++ NodeFactory.py
@@ -7,23 +7,28 @@ class AbstractNodeFactory():
     mConn = False
     def __init__(self):
         pass
-    def setConn(self):
-        pass
     def createTimer(self):
         pass
     def createConn(self):
         pass
+    def createLifeTime(self):
+        pass
 
+"""
+Decides all the parameter in the experiment, including
+the topology between nodes, message delay, and the
+life time of each nodes
+"""
 class SimpleNodeFactory(AbstractNodeFactory):
     def __init__(self, gap=1):
         self.mGap = gap
-        self.mConn = False
-    def setConn(self, connect):
-        self.mConn = connect
+        self.mConn = UniformConn(NoDelay())
     def createTimer(self):
         return TimeoutLinear(self.mGap)
     def createConn(self):
         return self.mConn
+    def createLifeTime(self):
+        return 5
 
 if __name__ == '__main__':
     factory = SimpleNodeFactory()
@@ -31,3 +36,4 @@ if __name__ == '__main__':
     factory.setConn(connect)
     t = factory.createTimer()
     c = factory.createConn()
+    duration = factory.createLifeTime()
